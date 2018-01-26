cecf3ddcc83465f58e0ecc374128ff38215e1847
Mark Lavin <mlavin@caktusgroup.com>
2018-01-26 13:40:41 -0500

    Allow changing the puzzle size.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 (function () {
+
     function Square(props) {
         return React.createElement('button', {'class': 'square', onClick: props.onClick},
             props.value !== null ? props.value : '');
@@ -6,11 +7,35 @@
 
     class Board extends React.Component {
 
+        renderSquare(i) {
+            return React.createElement(Square, {
+                value: this.props.puzzle[i],
+                onClick: () => this.props.handleClick(i)
+            });
+        }
+
+        render() {
+            const rows = [];
+            for (let i = 0; i < this.props.size; i++) {
+                let row = [];
+                for (let j = 0; j < this.props.size; j++) {
+                    row.push(this.renderSquare(i * this.props.size + j));
+                }
+                rows.push(
+                    React.createElement('div', {'class': 'board-row'}, row)
+                );
+            }
+            return React.createElement('div', {'class': 'board'}, rows);
+        }
+    }
+
+    class Game extends React.Component {
+
         constructor(props) {
             super(props);
             this.state = {
-                values: this.generatePuzzle(3),
-                size: 3
+                size: 4,
+                puzzle: this.generatePuzzle(4)
             };
         }
 
@@ -49,25 +74,18 @@
             return iterations % 2 === 0;
         }
 
-        renderSquare(i) {
-            return React.createElement(Square, {
-                value: this.state.values[i],
-                onClick: () => this.handleClick(i)
-            });
-        }
-
         handleClick(pos) {
-            const current = this.state.values.slice();
+            const current = this.state.puzzle.slice();
             const candidates = [];
             const q = Math.floor(pos / this.state.size);
             const r = pos % this.state.size;
-            if (q < 2) {
+            if (q < this.state.size - 1) {
                 candidates.push(pos + this.state.size);
             }
             if (q > 0) {
                 candidates.push(pos - this.state.size);
             }
-            if (r < 2) {
+            if (r < this.state.size - 1) {
                 candidates.push(pos + 1);
             }
             if (r > 0) {
@@ -79,29 +97,38 @@
                     current[pos] = null;
                 }
             });
-            this.setState({values: current});
+            this.setState({puzzle: current});
         }
 
-        render () {
-            return React.createElement('div', {},
-                React.createElement('div', {'class': 'board-row'},
-                    this.renderSquare(0),
-                    this.renderSquare(1),
-                    this.renderSquare(2)
+        onChange(value) {
+            const puzzle = this.generatePuzzle(value)
+            this.setState({size: value, puzzle: puzzle});
+        }
+
+        render() {
+            return React.createElement('div', {'class': 'game'},
+                React.createElement('div', {'class': 'controls'},
+                    React.createElement('label', {'for': 'boardsize'}, 'Puzzle Size'),
+                    React.createElement('input', {
+                        id: 'boardsize',
+                        name: 'boardsize',
+                        type: 'number',
+                        min: 2,
+                        max: 10,
+                        step: 1,
+                        value: this.state.size,
+                        onChange: (event) => this.onChange(parseInt(event.target.value, 10))
+                    })
                 ),
-                React.createElement('div', {'class': 'board-row'},
-                    this.renderSquare(3),
-                    this.renderSquare(4),
-                    this.renderSquare(5)
-                ),
-                React.createElement('div', {'class': 'board-row'},
-                    this.renderSquare(6),
-                    this.renderSquare(7),
-                    this.renderSquare(8)
-                )
+                React.createElement(Board, {
+                    size: this.state.size,
+                    puzzle: this.state.puzzle,
+                    handleClick: (pos) => this.handleClick(pos)
+                })
             );
         }
+
     }
 
-    ReactDOM.render(React.createElement(Board), document.getElementById('root'));
+    ReactDOM.render(React.createElement(Game), document.getElementById('root'));
 })();
