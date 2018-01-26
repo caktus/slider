abbf7c3f1684b30aef9ed4a58e0fafde990e8896
Mark Lavin <mlavin@caktusgroup.com>
2018-01-26 10:43:16 -0500

    Move pieces around on click.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 (function () {
     function Square(props) {
-        return React.createElement('button', {'class': 'square'},
+        return React.createElement('button', {'class': 'square', onClick: props.onClick},
             props.value !== null ? props.value : '');
     }
 
@@ -8,11 +8,43 @@
 
         constructor(props) {
             super(props);
-            this.state = [1, 2, 3, 4, 5, 6, 7, 8, null];
+            this.state = {
+                values: [1, 2, 3, 4, 5, 6, 7, 8, null],
+                size: 3
+            };
         }
 
         renderSquare(i) {
-            return React.createElement(Square, {value: this.state[i]});
+            return React.createElement(Square, {
+                value: this.state.values[i],
+                onClick: () => this.handleClick(i)
+            });
+        }
+
+        handleClick(pos) {
+            const current = this.state.values.slice();
+            let candidates = [];
+            let q = Math.floor(pos / this.state.size);
+            let r = pos % this.state.size;
+            if (q < 2) {
+                candidates.push(pos + this.state.size);
+            }
+            if (q > 0) {
+                candidates.push(pos - this.state.size);
+            }
+            if (r < 2) {
+                candidates.push(pos + 1);
+            }
+            if (r > 0) {
+                candidates.push(pos - 1);
+            }
+            candidates.forEach(function (i) {
+                if ((i >= 0) && (i < current.length) && (current[i] === null)) {
+                    current[i] = current[pos];
+                    current[pos] = null;
+                }
+            });
+            this.setState({values: current});
         }
 
         render () {
@@ -36,6 +68,5 @@
         }
     }
 
-    // ========================================
     ReactDOM.render(React.createElement(Board), document.getElementById('root'));
 })();
